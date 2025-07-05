3008f0369514eafddf03cc7a24306ade2a92e5f4
HIMU_BUTOLA <himu11071980@gmail.com>
2025-07-05 18:06:21 +0530

    First time yah...!

---
+++ First_one.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello world! \nAt least... thats what they always wnat me to say, when it's the first time!");
+    return 0;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Hi-world
-this is me
